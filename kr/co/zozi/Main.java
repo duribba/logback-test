d944ac4b3f98cdf3a43905b79513503bf1c77d65
duribba <woalbboy1@gmail.com>
2022-01-10 14:27:22 +0900

    first

---
+++ kr/co/zozi/Main.java
@@ -0,0 +1,17 @@
+package kr.co.zozi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+class Main {
+
+	private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+//		System.out.println("Hello world");
+		logger.info("Hello world");
+	}
+}
+
+
